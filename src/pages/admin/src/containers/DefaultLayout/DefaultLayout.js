c484ba301b28ba06c190b3df1bbe4191e64f3572
Неля Гаврильчик <gavrilchiknelia@gmail.com>
2020-02-22 16:23:46 +0200

    add npm packages

--- src/pages/admin/src/containers/DefaultLayout/DefaultLayout.js
+++ src/pages/admin/src/containers/DefaultLayout/DefaultLayout.js
@@ -4,16 +4,16 @@ import * as router from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 import {
-  // AppAside, видалити компонент
-  // AppFooter, видалити компонент
-  AppHeader,
-  AppSidebar,
-  AppSidebarFooter,
-  AppSidebarForm,
-  AppSidebarHeader,
-  AppSidebarMinimizer,
-  AppBreadcrumb2 as AppBreadcrumb,
-  AppSidebarNav2 as AppSidebarNav,
+	// AppAside,
+	// AppFooter,
+	// AppHeader,
+	AppSidebar,
+	AppSidebarFooter,
+	AppSidebarForm,
+	AppSidebarHeader,
+	AppSidebarMinimizer,
+	AppBreadcrumb2 as AppBreadcrumb,
+	AppSidebarNav2 as AppSidebarNav,
 } from '@coreui/react';
 import navigation from '../../_nav';
 // routes config
@@ -21,71 +21,76 @@ import routes from '../../routes';
 
 // const DefaultAside = React.lazy(() => import('./DefaultAside'));
 // const DefaultFooter = React.lazy(() => import('./DefaultFooter'));
-const DefaultHeader = React.lazy(() => import('./DefaultHeader'));
+// const DefaultHeader = React.lazy(() => import('./DefaultHeader'));
 
 class DefaultLayout extends Component {
+	loading = () => (
+		<div className="animated fadeIn pt-1 text-center">Loading...</div>
+	);
 
-  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
+	signOut(e) {
+		e.preventDefault();
+		this.props.history.push('/login');
+	}
 
-  signOut(e) {
-    e.preventDefault();
-    this.props.history.push('/login');
-  }
-
-  render() {
-    return (
-      <div className="app bg-gray-100">
-        <AppHeader fixed>
-          <Suspense  fallback={this.loading()}>
-            <DefaultHeader onLogout={e=>this.signOut(e)}/>
-          </Suspense>
-        </AppHeader>
-        <div className="app-body">
-          <AppSidebar fixed display="lg">
-            <AppSidebarHeader />
-            <AppSidebarForm />
-            <Suspense>
-            <AppSidebarNav navConfig={navigation} {...this.props} router={router}/>
-            </Suspense>
-            <AppSidebarFooter />
-            <AppSidebarMinimizer />
-          </AppSidebar>
-          <main className="main">
-            <AppBreadcrumb appRoutes={routes} router={router}/>
-            <Container fluid>
-              <Suspense fallback={this.loading()}>
-                <Switch>
-                  {routes.map((route, idx) => {
-                    return route.component ? (
-                      <Route
-                        key={idx}
-                        path={route.path}
-                        exact={route.exact}
-                        name={route.name}
-                        render={props => (
-                          <route.component {...props} />
-                        )} />
-                    ) : (null);
-                  })}
-                  <Redirect from="/" to="/companies" />
-                </Switch>
-              </Suspense>
-            </Container>
-          </main>
-          {/*<AppAside fixed>*/}
-          {/*  <Suspense fallback={this.loading()}>*/}
-          {/*    <DefaultAside />*/}
-          {/*  </Suspense>*/}
-          {/*</AppAside>*/}
-        </div>
-        {/*<AppFooter>*/}
-        {/*  <Suspense fallback={this.loading()}>*/}
-        {/*    <DefaultFooter />*/}
-        {/*  </Suspense>*/}
-        {/*</AppFooter>*/}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				{/*className="app bg-gray-100"*/}
+				{/*<AppHeader fixed>*/}
+				{/*  <Suspense  fallback={this.loading()}>*/}
+				{/*    <DefaultHeader onLogout={e=>this.signOut(e)}/>*/}
+				{/*  </Suspense>*/}
+				{/*</AppHeader>*/}
+				<div className="app-body">
+					<AppSidebar fixed display="lg">
+						<AppSidebarHeader />
+						<AppSidebarForm />
+						<Suspense>
+							<AppSidebarNav
+								navConfig={navigation}
+								{...this.props}
+								router={router}
+							/>
+						</Suspense>
+						<AppSidebarFooter />
+						<AppSidebarMinimizer />
+					</AppSidebar>
+					<main className="main">
+						<AppBreadcrumb appRoutes={routes} router={router} />
+						<Container fluid>
+							<Suspense fallback={this.loading()}>
+								<Switch>
+									{routes.map((route, idx) => {
+										return route.component ? (
+											<Route
+												key={idx}
+												path={route.path}
+												exact={route.exact}
+												name={route.name}
+												render={props => <route.component {...props} />}
+											/>
+										) : null;
+									})}
+									<Redirect from="/" to="/companies" />
+								</Switch>
+							</Suspense>
+						</Container>
+					</main>
+					{/*<AppAside fixed>*/}
+					{/*  <Suspense fallback={this.loading()}>*/}
+					{/*    <DefaultAside />*/}
+					{/*  </Suspense>*/}
+					{/*</AppAside>*/}
+				</div>
+				{/*<AppFooter>*/}
+				{/*  <Suspense fallback={this.loading()}>*/}
+				{/*    <DefaultFooter />*/}
+				{/*  </Suspense>*/}
+				{/*</AppFooter>*/}
+			</div>
+		);
+	}
 }
 
 export default DefaultLayout;
