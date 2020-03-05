9f87446b3a6eddb3994d14056048ecfe1bd879b1
Volodymyr <vbatsan@gmail.com>
2020-03-06 00:59:58 +0200

    change logout component

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -1,14 +1,30 @@
-import { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
+import { withRouter } from 'react-router-dom';
 
 import { logOut } from '../../store/auth/action/authActions';
 
+const useStyle = makeStyles(theme => ({
+	btn: {
+		background: 'none',
+		outline: 'none',
+		border: 'none',
+		color: theme.palette.color,
+		cursor: 'pointer',
+	},
+}));
+
 function LogOut(props) {
-	useEffect(() => {
-		props.logOut();
-		props.history.push('/login');
-	});
-	return null;
+	const classes = useStyle();
+	const { logOut } = props;
+	const { t } = useTranslation();
+	return (
+		<button className={classes.btn} onClick={() => logOut(props.history)}>
+			{t('EXIT')}
+		</button>
+	);
 }
 
-export default connect(null, { logOut })(LogOut);
+export default connect(null, { logOut })(withRouter(LogOut));
