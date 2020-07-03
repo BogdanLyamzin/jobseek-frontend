169dd30f8e6313bd0595cad5f509fc87b4720f57
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-07-03 12:58:50 +0300

    fix error with delete vacancy/hr

--- src/store/hr/actions.js
+++ src/store/hr/actions.js
@@ -1,3 +1,6 @@
+import API from 'services/api';
+import errorAxios from 'utils/actions/errorAxios';
+import tostrActions from 'utils/toastr/toastrAction';
 import actionFactory from 'utils/actions/actionFactory';
 import { GET, PUT, POST, DELETE } from 'utils/variables/method';
 import actionApiFactory from 'utils/actions/actionWithApiFactory';
@@ -9,7 +12,20 @@ const successAxiosHRList = actionFactory(SUCCESS_AXIOS_LIST);
 
 export const getOneHR = actionApiFactory('hr/', GET, successAxios);
 export const getAllHR = actionApiFactory('hr', GET, successAxiosHRList);
-export const deleteHR = actionApiFactory('hr/', DELETE, null, 'HR видалено');
 export const getHrByFilter = actionApiFactory('hr?', GET, successAxiosHRList);
 export const addHr = actionApiFactory('hr', POST, addUser, 'HR успішно створено');
 export const updateHR = actionApiFactory('hr/', PUT, successAxios, 'Інформацію оновлено');
+
+export const deleteHR = (id, arr) => dispatch => {
+	API[DELETE](`hr/${id}`)
+		.then(data => {
+			tostrActions(data, 'HR видалено');
+			if (data.status === 'Success') {
+				const newArr = arr.filter(el => el._id !== id);
+				dispatch(successAxiosHRList(newArr));
+			}
+		})
+		.catch(error => {
+			dispatch(errorAxios(error));
+		});
+};
--- src/store/vacancy/actions.js
+++ src/store/vacancy/actions.js
@@ -6,6 +6,9 @@ import {
 	GET_CANDIDATES,
 	SUCCESS_AXIOS_LIST,
 } from './actionNames';
+import API from 'services/api';
+import errorAxios from 'utils/actions/errorAxios';
+import tostrActions from 'utils/toastr/toastrAction';
 import actionFactory from 'utils/actions/actionFactory';
 import { GET, PUT, POST, DELETE } from 'utils/variables/method';
 import actionApiFactory from 'utils/actions/actionWithApiFactory';
@@ -25,7 +28,20 @@ const addMsg = 'Вакансію успішно створено';
 export const getOneVacancy = actionApiFactory('vacancies/', GET, successAxios);
 export const getAllVacancy = actionApiFactory('vacancies', GET, successAxiosList);
 export const addVacancy = actionApiFactory('vacancies', POST, addNewVacancy, addMsg);
-export const deleteVacancy = actionApiFactory('vacancies/', DELETE, null, deleteMsg);
 export const getVacancyByFilter = actionApiFactory('vacancies?', GET, successAxiosList);
 export const updateVacancy = actionApiFactory('vacancies/', PUT, successAxios, updateMsg);
 export const getSuitableCandidates = actionApiFactory('suitableCandidates/', GET, getCandidates);
+
+export const deleteVacancy = (id, arr) => dispatch => {
+	API[DELETE](`vacancies/${id}`)
+		.then(data => {
+			tostrActions(data, deleteMsg);
+			if (data.status === 'Success') {
+				const newArr = arr.filter(el => el._id !== id);
+				dispatch(successAxiosList(newArr));
+			}
+		})
+		.catch(error => {
+			dispatch(errorAxios(error));
+		});
+};
